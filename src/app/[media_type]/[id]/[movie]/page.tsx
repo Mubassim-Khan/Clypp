a3ef691b67b0ebc8f11e549b3e7bb77347383342
mubassim08 <mubassimahmedkhan@gmail.com>
2025-07-03 09:07:36 +0000

    fix: resolve build type errors

--- src/app/[media_type]/[id]/[movie]/page.tsx
+++ src/app/[media_type]/[id]/[movie]/page.tsx
@@ -3,33 +3,22 @@ import { notFound } from "next/navigation";
 import {
   fetchMovieDetails,
   fetchTvSeriesDetails,
-  fetchMovieTrailer,
-  fetchTvSeriesTrailer,
 } from "../../../../../lib/fetchMovie";
 import MovieDetailClient from "../../../../../components/MovieDetailClient";
 
-interface PageProps {
-  params: {
-    id: string;
-    media_type: string;
-  };
-}
-
-export default async function MovieDetailPage({ params }: PageProps) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function MovieDetailPage({ params }: any) {
   const { media_type, id } = params;
 
   let movie = null;
-  let trailer = null;
 
   if (media_type === "tv") {
     movie = await fetchTvSeriesDetails(id);
-    trailer = await fetchTvSeriesTrailer(Number(id));
   } else {
     movie = await fetchMovieDetails(id);
-    trailer = await fetchMovieTrailer(Number(id));
   }
 
   if (!movie) notFound();
 
-  return <MovieDetailClient movie={movie} trailer={trailer} />;
+  return <MovieDetailClient movie={movie} />;
 }
